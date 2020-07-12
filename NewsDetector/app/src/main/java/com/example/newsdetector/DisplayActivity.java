4fde211d331309e4f7b3d0f8c13227a9f78e2f52
Xumei Li <lixum2006@gmail.com>
2020-07-11 20:37:31 -0700

    hh  Pleas enter the commit message for your changes. Lines starting

--- NewsDetector/app/src/main/java/com/example/newsdetector/DisplayActivity.java
+++ NewsDetector/app/src/main/java/com/example/newsdetector/DisplayActivity.java
@@ -1,13 +1,19 @@
 package com.example.newsdetector;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.RatingBar;
 
+
 public class DisplayActivity extends AppCompatActivity {
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,5 +46,7 @@ public class DisplayActivity extends AppCompatActivity {
         analysisTextView.setText(reply.substring(4).trim());
         float rating = Float.valueOf(reply.substring(1, 4)) / 100 * 5;
         simpleRatingBar.setRating(rating);
+        float color = rating*(Color.RED - Color.YELLOW)/5 + Color.YELLOW;
+        simpleRatingBar.setProgressTintList(ColorStateList.valueOf((int) color));
     }
 }
