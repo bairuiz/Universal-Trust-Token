d3f9bb9bfdae0ee4dec3d6b4b74cefaeb9f3353a
xli1234 <lixum2006@gmail.com>
2020-07-26 22:06:14 -0400

    Update log path

--- Server/PythonServer/Log.py
+++ Server/PythonServer/Log.py
@@ -4,12 +4,12 @@ import sys
 
 dt = datetime.datetime.now()
 dirPath = '/home/xumeil/log/'
-logFile = dt.strftime('server_%Y%m%d_%H%M%S.log')
+logFile = dirPath + dt.strftime('server_%Y%m%d_%H%M%S.log')
 
 def log(*args):
     s = ' '.join(map(str,args))
     print(s)
-    f = open(dirPath+logFile, 'a+')
+    f = open(logFile, 'a+')
     f.write(s)
     f.write('\n')
     f.close()
